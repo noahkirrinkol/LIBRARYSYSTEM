18ff9adcb28de359f8614443b0edc32b6b918de6
noahkirrinkol <91315288+noahkirrinkol@users.noreply.github.com>
2021-11-29 12:54:25 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,24 @@
+/*
+   Project:  County Library System
+   Author:  Noah Kirrinkol Wuala
+   Date:     Oct, 2021
+   Compiler: GNC GCC
+   Language: C 99
+   License:  MIT
+
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("\tCounty Library System\n");
+    printf("Welcome Mr. Noah. \n");
+    printf("What would you like to do?");
+    printf("1. Add new User. \n");
+    printf("2. Add Book. \n");
+    printf("4. Search Book. \n");
+    printf("3. Borrow Book. \n");
+    printf("4. Return Book. \n");
+    return 0;
+}
